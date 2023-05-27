5d23dcb9356e7dddecdeb0d9e9e2a830e5911dd9
robypalmy <79595082+robypalmy@users.noreply.github.com>
2023-05-27 18:56:38 +0200

    created header and icon for hamburger menu

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,55 @@
 <template>
   <v-app>
-    <!--  <img alt="Vue logo" src="./assets/logo.png">-->
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    <InputFile/>
+    <v-app-bar app class="my-app-bar">
+      <HeaderApp/>
+    </v-app-bar>
+    <v-main>
+      <!--  <img alt="Vue logo" src="./assets/logo.png">-->
+      <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+      <InputFile/>
+    </v-main>
   </v-app>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
 import InputFile from './components/InputFile.vue'
+import HeaderApp from './components/HeaderApp.vue'
 
 export default {
   name: 'App',
   components: {
-    InputFile
+    InputFile,
+    HeaderApp
   }
 }
 </script>
 
-<style>
+<style scoped>
+
+
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 50px;
+  
+}
+
+
+#main {
+  display: flex;
+  align-items: center;
+  justify-content: center;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+
 }
+
+
+
 </style>
---
+++ frontend/src/components/HeaderApp.vue
@@ -0,0 +1,64 @@
+<template>
+  <header>
+    <div class="ciao">
+      <h1> Formula Hamilton </h1>
+    </div>
+    <v-btn icon>
+      <v-icon @click="toggleMenu">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>menu</title><path d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z" /></svg>
+      </v-icon>
+    </v-btn>
+  </header>
+</template>
+
+<script>
+
+export default {
+  name: 'HeaderApp',
+  methods: {
+    toggleMenu() {
+      // Handle menu toggle logic here
+    },
+  }
+}
+
+</script>
+
+<style scoped>
+
+
+
+header {
+  height: 100%;
+  width: 100%;
+  display: flex;
+  background-color: beige;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+}
+
+.ciao {
+  height: 100%;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+}
+
+
+h1 {
+  font-size: 40px;
+  font-weight: bold;
+  color: black;
+
+}
+
+.v-btn {
+  margin-right: 20px;
+  color: black;
+}
+ 
+
+</style>
